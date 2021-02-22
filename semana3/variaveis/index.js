d4b50d3706a9ef88731ef24a24878813be44c97c
Milena-Ribeiro <milenai@academico.faculdadeages.edu.br>
2021-02-22 16:27:20 -0300

    tirando comentario e resolvendo exercicio de variaveis

--- semana3/variaveis/index.js
+++ semana3/variaveis/index.js
@@ -9,53 +9,57 @@
 // 10, 20, a
 
 // Exercicio 1
-// let nome = 'Milena'
-// console.log(typeof nome)
-// let idade = 22
-// console.log(typeof idade)
+let nome = 'Milena'
+console.log(typeof nome)
+let idade = 22
+console.log(typeof idade)
 // d) Foi impresso underfined porque não foi atribuído valor pra variável. 
 // e) Com o valor atribuído e imprimindo as informações separadamente, aparecem as variáveis string e number.
 
-// const nome = prompt('Qual é o seu nome?')
-// const idade = prompt('Qual é a sua idade?')
+const nome = prompt('Qual é o seu nome?')
+const idade = prompt('Qual é a sua idade?')
 // d) Para os outros códigos funcionarem, é necessário comentar este 
 
 const apresentacao = 'Olá, Milena, você tem 22 anos'
 console.log(apresentacao)
 
 // Exercicio 2
-// const cor = prompt('Qual a sua cor preferida?')
+const cor = prompt('Qual a sua cor preferida?')
 const cor = '1. Qual a sua cor preferida?'
 const resposta = 'Resposta: Azul'
 console.log(cor)
 console.log(resposta)
-// const musica = prompt('Qual música você mais tem escutado?')
+
+const musica = prompt('Qual música você mais tem escutado?')
 const musica1 = '2. Qual música você mais tem escutado?'
 const musica2 = 'Resposta: Céu Azul'
 console.log(musica1)
 console.log(musica2)
-// const livro = prompt('Qual livro você está lendo?')
+
+const livro = prompt('Qual livro você está lendo?')
 const livro1 = '3. Qual livro você está lendo?'
 const livro2 = 'Resposta: Harry Potter'
 console.log(livro1)
 console.log(livro2)
-// const filme = prompt('Qual foi o último filme que você assistiu?')
+
+const filme = prompt('Qual foi o último filme que você assistiu?')
 const filme1 = '4. Qual foi o último livro que você leu?'
 const filme2 = 'Resposta: WW84'
 console.log(filme1)
 console.log(filme2)
-// const comida = prompt('Qual comida você mais gosta?')
+
+const comida = prompt('Qual comida você mais gosta?')
 const comida1 = '5. Qual comida você mais gosta?'
 const comida2 = 'Resposta: Sorvete'
 console.log(comida1)
 console.log(comida2)
 
 // Exercicio 3
-// let comidasPreferidas = ["pizza", "sorvete", "bolo", "café", "chocolate"];
-// console.log("Essas são as minhas comidas preferidas: \n" +  comidasPreferidas[0]+ "\n",  comidasPreferidas [1] +"\n",  comidasPreferidas[2] +"\n",  comidasPreferidas[3] +"\n",  comidasPreferidas [4] +"\n");
-// comidasPreferidas[2]='lasanha'
-// console.log("Essas são as minhas comidas preferidas: \n" +  comidasPreferidas[0]+ "\n",  comidasPreferidas [1] +"\n",  comidasPreferidas[2] +"\n",  comidasPreferidas[3] +"\n",  comidasPreferidas [4] +"\n");
-// const pergunta = prompt('Qual a sua comida preferida?')
+let comidasPreferidas = ["pizza", "sorvete", "bolo", "café", "chocolate"];
+console.log("Essas são as minhas comidas preferidas: \n" +  comidasPreferidas[0]+ "\n",  comidasPreferidas [1] +"\n",  comidasPreferidas[2] +"\n",  comidasPreferidas[3] +"\n",  comidasPreferidas [4] +"\n");
+comidasPreferidas[2]='lasanha'
+console.log("Essas são as minhas comidas preferidas: \n" +  comidasPreferidas[0]+ "\n",  comidasPreferidas [1] +"\n",  comidasPreferidas[2] +"\n",  comidasPreferidas[3] +"\n",  comidasPreferidas [4] +"\n");
+const pergunta = prompt('Qual a sua comida preferida?')
 
 // Exercicio 4
 let perguntas = ["Você está usando uma roupa azul hoje?", "Você está vestindo calça?", "Você tomou água hoje?"]
