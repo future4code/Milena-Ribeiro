b0864b5ed99133aca0abdf74b176d8f349ec7d3a
Milena-Ribeiro <milenai@academico.faculdadeages.edu.br>
2021-03-01 18:29:00 -0300

    resolvendo questao 5

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -10,18 +10,51 @@
 // No array final são adicionados o resultado dos números pares multuplicados por eles mesmos. Numeros, numerosPares.
 
 // Exercicio 4
-function sobreMim(){
-    console.log("Eu sou Milena, tenho 22 anos, moro em Jacobina e sou estudante.")
-}
-sobreMim()
+// function sobreMim(){
+//     console.log("Eu sou Milena, tenho 22 anos, moro em Jacobina e sou estudante.")
+// }
+// sobreMim()
 
-let amiga = (nome, idade, endereco, estudante) => {
-    nome = "Uana"
-    idade = 22
-    endereco = "Rua das Laranjeiras"
-    estudante = true
-    if (estudante === true) {
-        console.log('Eu sou ' + nome + ', tenho ' + idade + ' anos, moro na ' + endereco + ' e sou estudante.')
+// let amiga = (nome, idade, endereco, estudante) => {
+//     nome = "Uana"
+//     idade = 22
+//     endereco = "Rua das Laranjeiras"
+//     estudante = true
+//     if (estudante === true) {
+//         console.log('Eu sou ' + nome + ', tenho ' + idade + ' anos, moro na ' + endereco + ' e sou estudante.')
+//     }
+// }
+// amiga()
+
+// Exercicio 5
+// function somandoDoisNumeros (numero1, numero2){
+//     const resultado = numero1+numero2
+//     return resultado
+// }
+// resultado=somandoDoisNumeros(10, 10)
+// console.log(resultado)
+
+// function comparacao(valor1, valor2){
+//     if(valor1 >= valor2){
+//         console.log(valor1 + " é maior que " + valor2)
+//     } else {
+//         if(valor1 < valor2){
+//             console.log(valor1 + " é menor que " + valor2)
+//         } else {
+//             if(valor1 === valor2){
+//                 console.log(valor1 + "e" + valor2 + "são iguais.")
+//             }
+//         }
+//     }
+// }
+// resultado=comparacao(50, 20)
+
+function palavras(){
+    let c = 1
+    while (c <= 10){
+        console.log("Olá!")
+        c++
     }
+    // return c = palavras
 }
-amiga()
+palavras()
