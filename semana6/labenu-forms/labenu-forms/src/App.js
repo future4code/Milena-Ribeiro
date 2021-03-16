c3ceee5fc5749b4af9afbdfc64323eeb5e58d5b6
Milena-Ribeiro <milenai@academico.faculdadeages.edu.br>
2021-03-16 07:24:44 -0300

    alteracoes finais

---
+++ semana6/labenu-forms/labenu-forms/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Etapa1 from "./Components/Etapa1";
+import Etapa2 from "./Components/Etapa2";
+import Etapa3 from "./Components/Etapa3";
+import EtapaFinal from "./Components/EtapaFinal";
+
+
+class App extends React.Component {
+  state = {
+    etapa: 1,
+  };
+
+  irParaProximaEtapa = () => {
+    const paginaEtapa1 = this.state.etapa + 1
+    this.setState({ etapa: paginaEtapa1 })
+  }
+
+  // render () {
+    renderizaEtapa = () => {
+      switch (this.state.etapa) {
+        case 1:
+          return <Etapa1 />;
+        case 2:
+          return <Etapa2 />;
+        case 3:
+          return <Etapa3 />;
+        case 4:
+          return <EtapaFinal />;
+        default:
+          return <Etapa1 />;
+    }
+  };
+  render () {
+    return (
+      <div>
+          {this.renderizaEtapa()}
+          <button onClick={this.irParaProximaEtapa}>Próxima etapa</button>
+        </div>
+    )
+  }
+}
+  
+  
+  
+
+export default App;
