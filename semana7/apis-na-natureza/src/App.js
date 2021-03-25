e5e675d7e30a27a15f541208b47180265f4b7ec1
Milena-Ribeiro <milenai@academico.faculdadeages.edu.br>
2021-03-25 18:16:48 -0300

    alteracao

--- semana7/apis-na-natureza/src/App.js
+++ semana7/apis-na-natureza/src/App.js
@@ -3,11 +3,8 @@ import React from 'react';
 import axios from 'axios';
 
 export default class App extends React.Component {
-
   state = {
     atividade: {},
-    pokemons: [],
-    especiePokemon: "",
   }
 
   getAtividade = async () => {
@@ -19,39 +16,8 @@ export default class App extends React.Component {
     }
   }
 
-
-
-componentDidMount() {
-  this.getPokemon();
-}
-
-getPokemon = async () => {
-  try {
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10/')
-    this.setState({pokemons: response.data.results})
-  } catch (err) {
-    console.log(err.data.results)
-    alert('Tente novamente.')
-  }
-}
-
-especiePokemon = async (name) => {
-  try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?${name}`)
-    this.setState({ especiePokemon: response.data.species.name })
-  } catch (err) {
-    alert('Tente novamente.')
-  }
-}
-
-mudaSelect = (event) => {
-  const nomePokemon = event.target.value;
-  this.especiePokemon(nomePokemon)
-}
-
 render() {
     const { activity, type, participants } = this.state.atividade;
-
     return (
       <div className="App">
         <h2>Atividades</h2>
@@ -59,20 +25,7 @@ render() {
           <p>Número de participantes: {participants}</p>
           <p>Tipo: {type}</p>
         <button onClick={this.getAtividade}>Sortear atividade</button>
-        
-        <h2>Pokemon</h2>
-        <select onChange={this.mudaSelect}>
-          <option value={""}></option>
-          {this.state.pokemons.map((poke) => {
-            return <option key={poke.name} value ={poke.name}>
-            {poke.name}
-            </option>
-          })}
-        </select>
-        {this.state.especiePokemon}
       </div>
-
-
     );
   }
 }
