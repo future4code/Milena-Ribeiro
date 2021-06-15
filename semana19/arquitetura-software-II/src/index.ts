10560098f4b07cc15b274ade82652fe3ca01c5c2
Milena-Ribeiro <milenai@academico.faculdadeages.edu.br>
2021-06-15 16:14:04 -0300

    tirando comentários

--- semana19/arquitetura-software-II/src/index.ts
+++ semana19/arquitetura-software-II/src/index.ts
@@ -1,17 +1,6 @@
 import { app } from "./controller/app"
-// import { signup } from './controller/user/signup'
-// import { login } from './controller/user/login'
-// import { createTask } from './controller/task/createTask'
-// import { getTaskById } from './controller/task/getTaskById'
-
 import { taskRouter } from './routes/taskRouter'
 import { userRouter } from './routes/userRouter'
-
-// app.post('/user/signup', signup)
-// app.post('/user/login', login)
-
-// app.put('/task', createTask)
-// app.get('/task/:id', getTaskById)
 
 app.use('/task', taskRouter)
 app.use('/user', userRouter)
